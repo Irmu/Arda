ccba11b368fcfe9227e3128735c8287d918dc462
Lórien <34319284+irmu@users.noreply.github.com>
2019-07-16 22:03:46 +0100

    2019.07.16

--- script.module.universalscrapers/lib/universalscrapers/scraperplugins/ddown.py
+++ script.module.universalscrapers/lib/universalscrapers/scraperplugins/ddown.py
@@ -6,19 +6,20 @@ from ..common import get_rd_domains, filter_host,send_log,error_log
 from ..scraper import Scraper
 
 dev_log = xbmcaddon.Addon('script.module.universalscrapers').getSetting("dev_log")
-
 s = requests.session()
 User_Agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
 
+
 class ddown(Scraper):
     domains = ['https://directdownload.tv/']
-    name = "Direct Download"
+    name = "DirectDownload"
     sources = []
+
 
     def __init__(self):
         self.base_link = 'https://directdownload.tv/'
         self.sources = []
-            
+
 
     def scrape_episode(self,title, show_year, year, season, episode, imdb, tvdb, debrid = False):
         try:            
@@ -27,13 +28,9 @@ class ddown(Scraper):
                 return [] 
             season_url = "0%s"%season if len(season)<2 else season
             episode_url = "0%s"%episode if len(episode)<2 else episode
-
             start_url = 'https://directdownload.tv/api?key=4B0BB862F24C8A29&qualities/disk-480p,disk-1080p-x265,tv-480p,tv-720p,web-480p,web-720p,web-1080p,web-1080p-x265,movie-480p-x265,movie-1080p-x265&limit=50&keyword=%s+s%se%s' %(title.lower(),season_url,episode_url)
             start_url=start_url.replace(' ','%20')
-            #SEND2LOG(start_url)
-
             content = requests.get(start_url).content
-            #print 'content >> ' +content
             links=re.compile('"http(.+?)"',re.DOTALL).findall(content)
             count = 0
             for url in links:
@@ -60,16 +57,4 @@ class ddown(Scraper):
                 error_log(self.name,argument)
             return self.sources  
 
-    # def resolve(self, url):
 
-            # return url
-            
-
-        
-def SEND2LOG(Txt):
-    print ':::::::::::::::::::::::::::::::::::::::::::::::::'
-    print ':'
-    print ': LOG string: ' + (str(Txt))
-    print ':'
-    print ':::::::::::::::::::::::::::::::::::::::::::::::::'
-    return 
--- script.module.universalscrapers/lib/universalscrapers/scraperplugins/putlockers.py
+++ script.module.universalscrapers/lib/universalscrapers/scraperplugins/putlockers.py
@@ -1,88 +1,55 @@
-import requests,re,time
-import urllib, base64
-from ..jsunpack import unpack
-import xbmcaddon
-from ..scraper import Scraper
-from ..common import clean_title,clean_search,send_log,error_log
+
+import re,xbmcaddon,time,requests
+from universalscrapers.scraper import Scraper
+from universalscrapers.common import random_agent,send_log,error_log
+
+requests.packages.urllib3.disable_warnings()
+s = requests.session()
 dev_log = xbmcaddon.Addon('script.module.universalscrapers').getSetting("dev_log")
 
-a = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0'
-b = requests.Session()
-c = {'User-Agent':a}
-                                           
-class putlockers(Scraper):
-    domains = ['putlockers-hd.stream']
-    name = "Putlockers"
-    headache = []
- 
-    def __init__(self):
-        self.i = 'https://putlockers-hd.stream'
 
-    def scrape_movie(self, x, y, z, debrid = False):
+class watchstream(Scraper):
+    domains = ['putlockers.movie']
+    name = "Putlockers"
+    sources = []
+
+
+    def __init__(self):
+        self.base_link = 'https://www7.putlockers.movie/embed/'
+
+
+    def scrape_movie(self, title, year, imdb, debrid = False):
         try:
+            start_time = time.time()
+            get_link = self.base_link + '%s/' %(imdb)
+            headers={'User-Agent':random_agent(),'referrer':get_link}
+            data = {'tmp_chk':'1'}
+            html = requests.post(get_link,headers=headers,data=data,verify=False,timeout=5).content
+            try:
+                link = re.compile('<iframe src="(.+?)"',re.DOTALL).findall(html)[0]
+            except:
+                link = ''
             count = 0
-            start_headache = time.time() 
-            e = clean_search(x.lower())
-            l = '%s/?s=%s+%s' %(self.i,e.replace(' ','+'),y)
-#            print l
-            i = b.get(l,headers=c,timeout=5).content
-            f = re.findall('<div class="thumbnail animation-2">.+?href="(.+?)">.+?alt="(.+?)"',i)
-            for e, l in f:
-                d, g = re.findall('(.+?)\((.+?)\)',str(l))[0]
-                if clean_title(d) == clean_title(x) and g == y:
- #                   print e
-                    q = b.get(e).content
-                    r = re.findall('<iframe class="metaframe rptss" src="(.+?)"',q)[0]
-                    t = b.get(r).content
-                    u = re.findall("var tc = '(.+?)'.+?url: \"(.+?)\".+?\"_token\": \"(.+?)\".+?function.+?\(s\)(.+?)</script>",t,re.DOTALL)
-                    for v, w, xoxo, yoyo in u:
-                        o = self.get_x_token(v,yoyo)
-                        p = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0',
-                                   'Host':'gomostream.com',
-                                   'Referer':r,
-                                   'x-token':o}
-                        g = {'tokenCode':v,
-                                '_token':xoxo}
-                        l = b.post(w,headers=p,data=g).json()
-                        for a in l:
-                            if 'putvid' in a:
-                                z = b.get(a).content
-                                enjoy = re.findall("<script type='text/javascript'>(.+?)</script>",z,re.DOTALL)[0]
-                                enjoy = unpack(enjoy)
-                                enjoy = re.findall('sources:\["(.+?)"',str(enjoy))[0]
-                                count+=1
-                                self.headache.append({'source': 'Putvid', 'quality': 'Unknown - Probably good', 'scraper': self.name, 'url': enjoy,'direct': False})
-                            elif 'openload' in a or 'streamango' in a:
-                                if 'openload' in a:
-                                    something = 'Openload'
-                                elif 'streamango' in a:
-                                    something = 'Streamango'
-                                z = b.get(a).content
-                                fun = re.findall('"description" content="(.+?)"',z)[0]
-                                if '1080p' in fun:
-                                    somestuff = '1080p'
-                                elif '720p' in fun:
-                                    somestuff = '720p'
-                                else:
-                                    somestuff = 'SD'
-                                count+=1
-                                self.headache.append({'source': something, 'quality': somestuff, 'scraper': self.name, 'url': a,'direct': False})
+            if link != '':
+                try:
+                    chk = requests.get(link).content
+                    rez = re.compile('"description" content="(.+?)"',re.DOTALL).findall(chk)[0]
+                    if '1080' in rez:
+                        res='1080p'
+                    elif '720' in rez:
+                        res='720p'
+                    else:
+                        res ='DVD'
+                except: res = 'DVD'
+                count +=1
+                self.sources.append({'source': 'Openload', 'quality': res, 'scraper': self.name, 'url': link,'direct': False})
                 if dev_log=='true':
-                    end_it_all = time.time() - start_headache
-                    send_log(self.name,end_it_all,count,x,y)   
-            return self.headache
-        except Exception, argument:
-            print argument
+                    end_time = time.time() - start_time
+                    send_log(self.name,end_time,count,title,year)
+                return self.sources
+        except Exception, argument:        
             if dev_log == 'true':
                 error_log(self.name,argument)
             return self.sources
 
-    def get_x_token(self,ta,xb):
-        st,fi,a1,a2 = re.findall('slice\((.+?),(.+?)\).+?\+ "(.+?)"\+"(.+?)"',str(xb))[0]
-        split = ta[int(st):int(fi)]
-        rev = split[::-1]
-        to = str(rev)+a1+a2
-        return to
 
-
-#putlockerhd().scrape_movie('logan','2017','')
