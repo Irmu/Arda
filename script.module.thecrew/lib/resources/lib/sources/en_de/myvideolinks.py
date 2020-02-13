f3aa2c86319dc391b29527a73e926d2b206a1cca
Lórien <34319284+irmu@users.noreply.github.com>
2020-02-13 19:20:06 +0000

    2020.02.13

--- script.module.thecrew/lib/resources/lib/sources/en_de/myvideolinks.py
+++ script.module.thecrew/lib/resources/lib/sources/en_de/myvideolinks.py
@@ -28,7 +28,7 @@ class source:
         self.priority = 1
         self.language = ['en']
         self.domains = ['iwantmyshow.tk', 'myvideolinks.net']
-        self.base_link = 'https://see.home.kg/'
+        self.base_link = 'https://get.myvideolinks.net/'
         self.search_link = '?s=%s'
 
     def movie(self, imdb, title, localtitle, aliases, year):
